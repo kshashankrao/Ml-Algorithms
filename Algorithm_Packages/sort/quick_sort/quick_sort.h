fa7f01ee636c3385e2b23ba6f4bffb67111a8ba0
kshashankrao <shashank.kadapanatham@gmail.com>
2020-06-07 16:18:59 +0200

    Added ,quick sort

---
+++ Algorithm_Packages/sort/quick_sort/quick_sort.h
@@ -0,0 +1,4 @@
+/* Algorithm:
+	Take the last element of an array as a pivot and put smaller elements before the pivot, and greater elements after the pivot.
+	Repeat it till no values can be partitioned.*/
+void quick_sort(int arr[], int low, int high);
