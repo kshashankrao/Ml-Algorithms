49cc16550628ad29b3a6d91b45c4b8b06040ed00
kshashankrao <shashank.kadapanatham@gmail.com>
2020-12-27 07:00:25 +0100

    Added comments

--- algorithms/image_proc_algorithms/histogram_equalization/histogram_equalization.h
+++ algorithms/image_proc_algorithms/histogram_equalization/histogram_equalization.h
@@ -4,10 +4,38 @@
 class Histogram_Equalization
 {
 public:
+	/*
+	* Brief: Main function to trigger
+	* Input: Image
+	* Output: Displays intensity equalized image
+	*/
 	void process(const cv::Mat& im);
+
 private:
+	/*
+	* Brief: Computes the histogram based on the intensity of the grayscale image
+	* Input: Image
+	* Output: Vector of Histogram
+	*/
 	std::vector<int> get_hist(const cv::Mat& image);
+
+	/*
+	* Brief: Calculates the normalized histogram
+	* Input: Image, histogra
+	* Output: Vector of normalized hist
+	*/
 	std::vector<float> normalize_hist(const cv::Mat& image, const std::vector<int> &hist);
+
+	/*
+	* Brief: Computes the cdf of the histogram
+	* Input: Normalized histogram
+	* Output: Vector of cdf
+	*/
 	std::vector<float> get_CDF(const std::vector<float> &norm_hist);
+	
+	/*
+	* Brief: Replaces the image pixels with newly calculated values
+	* Input: Image, cdf histogram
+	*/
 	void apply_equalization(cv::Mat &image, const std::vector<float>& cumulative_hist);
 };
