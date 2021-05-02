9f4ec1786516f447a039b101b983cb28f84fd784
kshashankrao <shashank.kadapanatham@gmail.com>
2021-05-02 02:08:03 +0200

    minor changes

--- python/optimizers/optimizers.py
+++ python/optimizers/optimizers.py
@@ -1,35 +1,45 @@
 class optimizers:
-    def __init__():
-        """! Constructor to initialize the parameters for optimizers"""
+    """! Class to implement different optimizers to minimize the loss and update the parameters"""
+
+    def __init__(self, loss_type):
+        """! Constructor to initialize the parameters for optimizers
+        @ param loss_type   Type of loss 
+        """
         optimizer_type = None
+        self.loss_type = loss_type
 
     def gradient_descent(self, w, b, X, Y, lr):
         """! Gradient descent updates the parameters (weight and bias) by calculating the partial 
         derivative of the loss function
         
-        @param Parameter 1 (Weight)
-        @param Parameter 2 (Bias)
-        @param Input vector
-        @param Actual output
-        @param Learning rate
-        @returns Updated parameter 1
-        @returns Updated parameter 2
+        @param  w    Parameter 1 (Weight)
+        @param  b    Parameter 2 (Bias)
+        @param  X    Input vector
+        @param  Y    Actual output
+        @param  lr   Learning rate
+        @returns    Updated parameter 1
+        @returns    Updated parameter 2
         """
-        w_ = 0
-        b_ = 0
+        dw = 0
+        db = 0
         N = len(X)
         
         # Partial derivative of the cost function (y_actual - y_pred)^2 i.e mean square error
         # dJ(w)/dw = -2x(y - (mx + b))
         # dJ(b)/db = -2(y - (mx + b)) 
         # TODO Implement for different cost function (ex. cross entropy loss and other popular loss fn)
+        # TODO Implement GD with momentum
         
         for i in range(N):
-            w_ += -2 * X[i] * (Y[i] - (w * X[i] + b))
-            b_ += -2 * (Y[i] - (w * X[i] + b))
+            dw += -2 * X[i] * (Y[i] - (w * X[i] + b))
+            db += -2 * (Y[i] - (w * X[i] + b))
 
-        w -= (m_deriv / N) * lr
-        b -= (b_deriv / N) * lr
+        # Update the parameters p_new = p_old - (lr * partial_derivative(J(p))
+        w -= (dw / N) * lr
+        b -= (db / N) * lr
 
         return w, b
-            
+    
+    def stochastic_gradient_descent(self, w, b, X, Y, lr):
+        """! Not implemented as it is similar to gradient descent but with a batch of input data"""
+        raise NotImplementedError(self.__class__.__name__ + '.stochastic_gradient_descent()')
