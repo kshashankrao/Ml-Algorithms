45aacf1c8437c990bead33382ab9eb0d7076a0e6
kshashankrao <shashank.kadapanatham@gmail.com>
2022-10-06 01:36:23 +0200

    Adapted the folder structure to make a cmake library for the algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "image_proc_algorithms/best_rect_fit/best_rect_fit.h"
 #include <opencv2/imgcodecs.hpp>
-
+#include "image_processing/best_rect_fit/best_rect_fit.h"
+//#include "dummy/"
 int main(int argc, char** argv)
 {
 	best_rect_fit b;
-	cv::Mat image = cv::imread(argv[1]);
+	cv::Mat image = cv::imread("D:\DeepLearning\Algorithms\Machine_Learning\data\blob_11.png");
 	b.process(image);
 	return 0;
 }
